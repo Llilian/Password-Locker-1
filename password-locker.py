d61eba1b3a43b7509633a689cd96249d7a2301fd
zecollokaris <collo.kariss@gmail.com>
2018-04-03 12:17:36 +0300

    added profile

---
+++ password-locker.py
@@ -0,0 +1,14 @@
+class Password:
+    """
+class that generates password and stores it
+    """
+
+    user_profile =[] #Empty profile
+    def __init__(self,first_name,last_name,username,password):
+        
+        self.first_name =first_name
+        self.last_name = last_name
+        self.username = username
+        self.password = password
+
+    
