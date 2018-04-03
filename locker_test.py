7884374083f4babf37aaaf234746cd85c6f0fe22
zecollokaris <collo.kariss@gmail.com>
2018-04-03 16:04:18 +0300

    added comment

--- locker_test.py
+++ locker_test.py
@@ -3,13 +3,25 @@ from passwordlocker import Password
 
 class TestPassword(unittest.TestCase):
 
+    '''
+     
+    Test class that defines test cases for the contact class behaviours.
+
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    
+
+    '''
+
     def setUp(self): 
             self.new_Password = Password("Collins","Kariuki","collo","karizy")
             
     def test_init(self):
     
 
-        self.assertEqual(self.new_Password.first_name,"faith")
-        self.assertEqual(self.new_Password.last_name,"thuita")
-        self.assertEqual(self.new_Password.username,"fait")
-        self.assertEqual(self.new_Password.password,"faiy")
+        self.assertEqual(self.new_Password.first_name,"Collins")
+        self.assertEqual(self.new_Password.last_name,"Kariuki")
+        self.assertEqual(self.new_Password.username,"collo")
+        self.assertEqual(self.new_Password.password,"karizy")
+if __name__ == '__main__':
+    unittest.main()
