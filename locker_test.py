bce84b3801987d7c2c97415a602174cb24e30a28
zecollokaris <collo.kariss@gmail.com>
2018-04-03 15:25:33 +0300

    corrected test setUp

--- locker_test.py
+++ locker_test.py
@@ -6,3 +6,10 @@ class TestPassword(unittest.TestCase):
     def setUp(self): 
             self.new_Password = Password("Collins","Kariuki","collo","karizy")
             
+    def test_init(self):
+    
+
+        self.assertEqual(self.new_Password.first_name,"faith")
+        self.assertEqual(self.new_Password.last_name,"thuita")
+        self.assertEqual(self.new_Password.username,"fait")
+        self.assertEqual(self.new_Password.password,"faiy")
