70d334f95e6eb1d2738a87dbf626dbd2c071eaec
zecollokaris <collo.kariss@gmail.com>
2018-04-04 14:37:22 +0300

    created new class

---
+++ user-data.py
@@ -0,0 +1,12 @@
+class Userdata:
+    
+    """class accepts user input"""
+
+    user_profile = []  # Empty profile
+
+    def __init__(self, first_name, last_name, username, password):
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.username = username
+        self.password = password
