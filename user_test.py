ed3b722455f4dd5c143e9f9d2818d022d73516cb
zecollokaris <collo.kariss@gmail.com>
2018-11-04 17:08:25 +0300

    another test mosified

--- user_test.py
+++ user_test.py
@@ -6,8 +6,8 @@ class TestUser(unittest.TestCase):
         self.new_user = User("Collins","Kariuki","collins@gmail.com",)
     def test1(self):
         self.assertEqual(self.new_user.f_name,"Collins")
-        self.assertEqual(self.new_user.m_name,"Anne")
-        self.assertEqual(self.new_user.e_mail,"mary@gmail.com")
+        self.assertEqual(self.new_user.m_name,"Kariuki")
+        self.assertEqual(self.new_user.e_mail,"collins@gmail.com")
     def test_save_user(self):
         self.new_user.save_user()
 
